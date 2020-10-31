4d753281e2dc4add7915138db9c67796e7c02cd5
Akshay Bengani <akshaybengani@gmail.com>
2020-10-31 18:17:53 +0530

    Create prepareDB.sh

---
+++ prepareDB.sh
@@ -0,0 +1,13 @@
+rm -rf /home/akshaybengani123/earning_check
+mkdir /home/akshaybengani123/earning_check
+sudo -i
+cp /mnt/storj-datastore/crypt-1/storage/bandwidth.db /home/akshaybengani123/earning_check/
+cp /mnt/storj-datastore/crypt-1/storage/storage_usage.db /home/akshaybengani123/earning_check/
+cp /mnt/storj-datastore/crypt-1/storage/piece_spaced_used.db /home/akshaybengani123/earning_check/
+cp /mnt/storj-datastore/crypt-1/storage/reputation.db /home/akshaybengani123/earning_check/
+cp /mnt/storj-datastore/crypt-1/storage/heldamount.db /home/akshaybengani123/earning_check/
+cd /home/akshaybengani123/
+git clone https://github.com/akshaybengani/storj_earnings
+mv earning_check storj_earnings/
+cd storj_earnings
+python3 earnings.py earning_check/
