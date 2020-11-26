0bf292d641945183937d30790b59a1ef51f47c70
Akshay Bengani <akshaybengani@gmail.com>
2020-11-26 23:13:25 +0530

    Update prepareDB.sh

--- prepareDB.sh
+++ prepareDB.sh
@@ -1,11 +1,11 @@
 rm -rf /home/akshaybengani123/earning_check
 mkdir /home/akshaybengani123/earning_check
 sudo -i
-cp /mnt/storj-datastore/crypt-1/storage/bandwidth.db /home/akshaybengani123/earning_check/
-cp /mnt/storj-datastore/crypt-1/storage/storage_usage.db /home/akshaybengani123/earning_check/
-cp /mnt/storj-datastore/crypt-1/storage/piece_spaced_used.db /home/akshaybengani123/earning_check/
-cp /mnt/storj-datastore/crypt-1/storage/reputation.db /home/akshaybengani123/earning_check/
-cp /mnt/storj-datastore/crypt-1/storage/heldamount.db /home/akshaybengani123/earning_check/
+cp /mnt/storj-datastore/crypt-*/storage/bandwidth.db /home/akshaybengani123/earning_check/
+cp /mnt/storj-datastore/crypt-*/storage/storage_usage.db /home/akshaybengani123/earning_check/
+cp /mnt/storj-datastore/crypt-*/storage/piece_spaced_used.db /home/akshaybengani123/earning_check/
+cp /mnt/storj-datastore/crypt-*/storage/reputation.db /home/akshaybengani123/earning_check/
+cp /mnt/storj-datastore/crypt-*/storage/heldamount.db /home/akshaybengani123/earning_check/
 cd /home/akshaybengani123/
 git clone https://github.com/akshaybengani/storj_earnings
 mv earning_check storj_earnings/
